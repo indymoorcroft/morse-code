ba3c1bf729825e629031bcc868c0506ceaf51afe
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2025-07-14 15:07:08 +0100

    Added function to convert valid characters into morse code with testing and exception handling

---
+++ src/main/scala/org/morseCode/Translator.scala
@@ -0,0 +1,22 @@
+package org.morseCode
+
+object Translator {
+
+  def englishToMorse(input: String): Either[String, String] = {
+    val convertedToMorse = input.toUpperCase.map {
+      case char if MorseCode.morseCode.contains(char) => Right(MorseCode.morseCode(char))
+      case c => Left(s"Invalid character: $c")
+    }
+
+    combineResults(convertedToMorse.toList).map(_.mkString(" "))
+  }
+
+  private def combineResults(results: List[Either[String, String]]): Either[String, List[String]] = {
+    results.foldRight(Right(Nil): Either[String, List[String]]) { (current, accumulated) =>
+      for {
+        valid <- current
+        rest <- accumulated
+      } yield valid :: rest
+    }
+  }
+}
---
+++ src/test/scala/org/morseCode/TranslatorTest.scala
@@ -0,0 +1,27 @@
+package org.morseCode
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class TranslatorTest extends AnyFunSuite {
+
+  // englishToMorse
+  test("English letter returns correct morse code") {
+    assert(Translator.englishToMorse("H").contains("...."))
+  }
+
+  test("English word returns correct morse code") {
+    assert(Translator.englishToMorse("HELLO").contains(".... . .-.. .-.. ---"))
+  }
+
+  test("Two english words with space returns correct morse code") {
+    assert(Translator.englishToMorse("HELLO WORLD").contains(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."))
+  }
+
+  test("Lowercase english letter returns correct morse code") {
+    assert(Translator.englishToMorse("h").contains("...."))
+  }
+
+  test("Invalid character returns left projection error") {
+    assert(Translator.englishToMorse("HI!").isLeft)
+  }
+}
