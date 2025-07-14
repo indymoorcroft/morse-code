b38f63d988a47cb83b8608bc1bbdfc9f9fd318e4
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2025-07-14 17:27:13 +0100

    added morse code to english function with testing and exception handling

--- src/main/scala/org/morseCode/Translator.scala
+++ src/main/scala/org/morseCode/Translator.scala
@@ -1,22 +1,37 @@
 package org.morseCode
 
+import org.morseCode.MorseCode.*
+
 object Translator {
 
   def englishToMorse(input: String): Either[String, String] = {
     val convertedToMorse = input.toUpperCase.map {
-      case char if MorseCode.morseCode.contains(char) => Right(MorseCode.morseCode(char))
+      case char if morseCode.contains(char) => Right(morseCode(char))
       case c => Left(s"Invalid character: $c")
     }
 
     combineResults(convertedToMorse.toList).map(_.mkString(" "))
   }
 
-  private def combineResults(results: List[Either[String, String]]): Either[String, List[String]] = {
-    results.foldRight(Right(Nil): Either[String, List[String]]) { (current, accumulated) =>
+  def morseToEnglish(input: String): Either[String, String] = {
+    val words = input.trim.split(" / ").toList
+
+    val convertedToLetters = words.map((word) => {
+      val chars = word.split(" ").toList.map((char) => {
+        inverseMorseCode.get(char).toRight(s"Invalid Morse code: $char")
+      })
+      combineResults(chars).map(_.mkString)
+    })
+
+    combineResults(convertedToLetters).map(_.mkString(" "))
+  }
+
+  private def combineResults[A](results: List[Either[String, A]]): Either[String, List[A]] = {
+    results.foldRight(Right(Nil): Either[String, List[A]]) { (current, accumulated) =>
       for {
         valid <- current
         rest <- accumulated
-      } yield valid :: rest // prepends
+      } yield valid :: rest
     }
   }
 }
--- src/test/scala/org/morseCode/TranslatorTest.scala
+++ src/test/scala/org/morseCode/TranslatorTest.scala
@@ -1,27 +1,45 @@
 package org.morseCode
 
+import org.morseCode.Translator.*
 import org.scalatest.funsuite.AnyFunSuite
 
 class TranslatorTest extends AnyFunSuite {
 
   // 1. englishToMorse
   test("English letter returns correct morse code") {
-    assert(Translator.englishToMorse("H").contains("...."))
+    assert(englishToMorse("H").contains("...."))
   }
 
   test("English word returns correct morse code") {
-    assert(Translator.englishToMorse("HELLO").contains(".... . .-.. .-.. ---"))
+    assert(englishToMorse("HELLO").contains(".... . .-.. .-.. ---"))
   }
 
   test("Two english words with space returns correct morse code") {
-    assert(Translator.englishToMorse("HELLO WORLD").contains(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."))
+    assert(englishToMorse("HELLO WORLD").contains(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."))
   }
 
   test("Lowercase english letter returns correct morse code") {
-    assert(Translator.englishToMorse("h").contains("...."))
+    assert(englishToMorse("h").contains("...."))
   }
 
   test("Invalid character returns left projection error") {
-    assert(Translator.englishToMorse("HI!").isLeft)
+    assert(englishToMorse("HI!").isLeft)
+  }
+
+  // 2. morseToEnglish
+  test("Morse code letter returns correct english letter") {
+    assert(morseToEnglish("....").contains("H"))
+  }
+
+  test("Morse code word returns correct english word") {
+    assert(morseToEnglish(".... . .-.. .-.. ---").contains("HELLO"))
+  }
+
+  test("Two morse code words returns correct english words") {
+    assert(morseToEnglish(".... . .-.. .-.. --- / .-- --- .-. .-.. -..").contains("HELLO WORLD"))
+  }
+
+  test("Invalid morse code returns left projection error") {
+    assert(morseToEnglish("....----..").isLeft)
   }
 }
