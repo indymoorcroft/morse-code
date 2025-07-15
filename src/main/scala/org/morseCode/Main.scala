f0742ab6f98af17b2593a42d5086e45fe5125948
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2025-07-15 12:46:11 +0100

    refactored getValidUser function to be reusuable and added functionality for user to continue to do translations until they want to exit the program

--- src/main/scala/org/morseCode/Main.scala
+++ src/main/scala/org/morseCode/Main.scala
@@ -4,15 +4,28 @@ import org.morseCode.InputHandler.*
 import org.morseCode.OutputHandler.displayResult
 import org.morseCode.Translator.*
 
+import scala.annotation.tailrec
+
 object Main extends App {
-  private val mode = translationPicker()
 
-  val input = readInput(if(mode == "1") "\nYou have chosen English to Morse. Enter English text:" else "\nYou have chosen Morse to English. Enter Morse code:")
+  @tailrec
+  private def runTranslator(): Unit = {
+    val mode = translationPicker()
 
-  val result = mode match {
-    case "1" => englishToMorse(input)
-    case "2" => morseToEnglish(input)
+    val input = readInput(if (mode == "1") "\nYou have chosen English to Morse. Enter English text:" else "\nYou have chosen Morse to English. Enter Morse code:")
+
+    val result = mode match {
+      case "1" => englishToMorse(input)
+      case "2" => morseToEnglish(input)
+    }
+
+    displayResult(result)
+
+    val continue = getValidInput(Set("y", "n"), "\nWould you like to do another translation? (y/n): ")
+
+    if(continue == "y") runTranslator()
+    else println("\nGoodbye!")
   }
 
-  displayResult(result)
+  runTranslator()
 }
