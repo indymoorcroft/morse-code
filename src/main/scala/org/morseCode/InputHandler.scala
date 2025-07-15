72f966012d3a2497f4b13f362e8fad4d46888eb5
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2025-07-15 11:08:27 +0100

    added recursive loop to keep asking user until they provide a valid input

--- src/main/scala/org/morseCode/InputHandler.scala
+++ src/main/scala/org/morseCode/InputHandler.scala
@@ -1,15 +1,35 @@
 package org.morseCode
 
+import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
+
 object InputHandler {
 
   def translationPicker(): String = {
     println("Choose mode:\n1. English to Morse\n2. Morse to English")
-    val input = scala.io.StdIn.readLine().trim
-    input match {
-      case "1" => "English to Morse"
-      case "2" => "Morse to English"
-      case _ => "Invalid option. Try again"
+    def attempt(): Try[String] = Try {
+      val input = scala.io.StdIn.readLine().trim
+
+      if(input != "1" && input != "2") {
+        throw new InvalidInputException("Please enter 1 or 2")
+      }
+
+      input match {
+        case "1" => "English to Morse"
+        case "2" => "Morse to English"
+      }
     }
+
+    @tailrec
+    def loop(): String = {
+      attempt() match {
+        case Success(c) => c
+        case Failure(e) =>
+          println(s"Invalid: ${e.getMessage}")
+          loop()
+      }
+    }
+    loop()
   }
 
   def readInput(prompt: String): String = {
@@ -17,4 +37,5 @@ object InputHandler {
     scala.io.StdIn.readLine()
   }
 
+  private class InvalidInputException(msg: String) extends Exception(msg)
 }
